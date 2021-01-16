317eb29b61499ceabb1e598ceb2992ad0da82e4f
Daniel Brestoiu <Daniel.Brestoiu@gmail.com>
2021-01-16 13:09:16 -0500

    Initial creation v2

---
+++ Nutrition Tracker/Nutrition Tracker/Nutrition_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nutrition_TrackerApp.swift
+//  Nutrition Tracker
+//
+//  Created by Daniel Brestoiu on 2021-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Nutrition_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
